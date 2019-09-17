8680fd6037ad7117dc2de260266020c2944039e9
Alexey Ershov <alex4rom@gmail.com>
2019-09-17 23:07:28 +0300

    Continue with win32 window

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 pushd build
 cl -Zi ..\main.cpp user32.lib gdi32.lib
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,72 @@
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+
+    if(BitmapMemory)
+    {
+        VirtualFree(0, 0, MEM_RELEASE);
+    }
+
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biComplression = BI_RGB;
+
+    int BytePerPixel = 4;
+    int BitmapMemorySize = BitmapWidth * BitmapHeight * BytePerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height)
+{
+    // int StretchDIBits(
+    //   HDC              hdc,
+    //   int              xDest,
+    //   int              yDest,
+    //   int              DestWidth,
+    //   int              DestHeight,
+    //   int              xSrc,
+    //   int              ySrc,
+    //   int              SrcWidth,
+    //   int              SrcHeight,
+    //   const VOID       *lpBits,
+    //   const BITMAPINFO *lpbmi,
+    //   UINT             iUsage,
+    //   DWORD            rop
+    // );
+
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
+
+    StretchDIBits(DeviceContext,
+                    // X, Y, Width, Height,
+                    // X, Y, Width, Height,
+                    0, 0, BitmapWidth, BitmapHeight,
+                    0, 0, WindowWidth, WindowHeight,
+                    BitmapMemory,
+                    &BitmapInfo,
+                    DIB_RGB_COLORS,
+                    SRCCOPY);
+}
+
 LRESULT CALLBACK MainWindowCallback(
     HWND   hwnd,
     UINT   uMsg,
@@ -17,12 +84,13 @@ LRESULT CALLBACK MainWindowCallback(
 
         case WM_DESTROY:
         {
+            Running = false;
             OutputDebugStringA("WM_DESTROY\n");
         } break;
 
         case WM_CLOSE:
         {
-            PostQuitMessage(0);
+            Running = false;
             OutputDebugStringA("WM_CLOSE\n");
         } break;
 
@@ -39,8 +107,9 @@ LRESULT CALLBACK MainWindowCallback(
             int Y = Paint.rcPaint.top;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
-            EndPaint(hwnd, &Paint);
+            // PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+            // EndPaint(hwnd, &Paint);
         } break;
 
         default:
@@ -83,7 +152,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLin
         if(WindowHandle)
         {
             MSG Message;
-            while(1)
+            Running = true;
+            while(Running)
             {
                 BOOL MsgResult = GetMessage(&Message, 0, 0, 0);
                 if(MsgResult > 0)
